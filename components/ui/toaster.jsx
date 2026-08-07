02bab59894c4467b2fdd79577d8c55e25f97dcb4
Rafael Bani <rafael.bani@rajagadai.id>
2026-08-07 14:47:04 +0700

    fix: resolve toaster positionerProps crash

--- components/ui/toaster.jsx
+++ components/ui/toaster.jsx
@@ -7,6 +7,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
+  ToastContent,
 } from "@/components/ui/toast"
 import { useToast } from "@/hooks/use-toast"
 
@@ -18,14 +19,16 @@ export function Toaster() {
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
+            <ToastContent className="flex flex-col gap-1 w-full relative border bg-background p-4 pr-8 text-foreground shadow-lg sm:rounded-xl">
+              <div className="grid gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
+              {action}
+              <ToastClose />
+            </ToastContent>
           </Toast>
         )
       })}
